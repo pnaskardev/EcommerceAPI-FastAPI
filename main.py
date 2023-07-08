044750c19ed9b35036ba88ca51b32e404ec409e7
pnaskardev <priyanshunaskar89@gmail.com>
2023-07-08 16:51:06 +0530

    adding products is succesfull

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI, Response, status
 import motor.motor_asyncio
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
+from models.product import Product
 from config.config import settings
 
 app = FastAPI()
-client=motor.motor_asyncio.AsyncIOMotorClient(settings.mongo_url)
+client = motor.motor_asyncio.AsyncIOMotorClient(settings.mongo_url)
+db = client.ecommerce
 
 
 @app.get("/")
-def read_root():
+async def read_root():
+    # list1=await client.list_database_names()
+    # list2=await db.list_collection_names()
+    # return {"list1": list1,"list2":list2}
+    
     return {"Hello": "World"}
+
+@app.post("/products/",response_description="Add new product", response_model=Product)
+async def create_product(product: Product=Body(...)):
+    product = jsonable_encoder(product)
+    new_product = await db["products"].insert_one(product)
+    created_student = await db["products"].find_one({"_id": new_product.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_student)
--- models/product.py
+++ models/product.py
@@ -1,7 +1,36 @@
-from typing import Union
-from pydantic import BaseModel
+from bson import ObjectId
+from pydantic import BaseModel, Field
+import json
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
 
 class Product(BaseModel):
-    name: str
-    price: float
-    quantity: int = 0
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "name": "Example Product",
+                "price": 10.99,
+                "quantity": 5,
+            }
+        }
+        populate_by_name = True
+        arbitrary_types_allowed = True
+        exclude = {"id"}
+
+    id: str = Field(default_factory=lambda: str(ObjectId()), alias="_id")
+    name: str = Field(...)
+    price: float = Field(..., gt=0, description="The price must be greater than zero")
+    quantity: int = Field(..., gt=0, description="The quantity must be greater than zero")
+
+    def json(self, *args, **kwargs):
+        kwargs["default"] = lambda o: str(o) if isinstance(o, ObjectId) else o
+        return json.dumps(self.dict(), *args, **kwargs)
